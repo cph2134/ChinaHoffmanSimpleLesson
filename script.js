b462e94f1ba7e96504239995fa353a94ab2ce500
China Hoffman <china.hoffman@gmail.com>
2017-06-08 16:37:25 -0400

    more test updates

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
-// Code goes here
+////Javasript for student page //
 
+//selectExcerpt
+//when user clicks on one of the radio buttons in exercise 2, it should hide the
+//excerpts not selected so it is easier to navigate the page.
+
+//saveAnswer
+//save selected text to a variable
+
+//submitAnswer
+//not sure we know how to do this - where would I send the text?
